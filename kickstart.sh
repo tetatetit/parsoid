32cf263caa39943083b2320ce4e69094e62571a1
Toma Luca <toma.luca95@gmail.com>
2016-01-10 21:19:12 +0100

    Created configuration-generator script (make.sh) and created localsettings.js using the script

--- kickstart.sh
+++ kickstart.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-if [[ "$langs" != "" ]] ; then
- {
-  echo "'use strict';"
-
-  echo "exports.setup = function(parsoidConfig) {"
-  for lang in $langs ; do
-  echo " parsoidConfig.setMwApi({ prefix: '${lang}wikitolearn', uri: 'http://$lang.wikitolearn.org/api.php' });"
-  done
-  echo " parsoidConfig.loadWMF = false;"
-  echo " parsoidConfig.useSelser = true;"
-  echo "};"
- } > /parsoid/api/settings.js
-fi
 node api/server.js
